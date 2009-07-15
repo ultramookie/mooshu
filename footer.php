547e194fa9cd015bbccd5adea333c23603052bda
Mookie Kong <mookie@ymail.com>
2009-07-15 16:41:44 -0700

    changed link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <hr />
-powered by <a href="http://ultrmookie.com">mooshu</a> written by steve "<a href="http://ultramookie.com">mookie</a>" kong.<br />
-<a href="http://ultramookie.com">mooshu</a> is distributed under <a href="http://www.gnu.org/licenses/gpl-3.0.html">gplv3</a>.<br />
+powered by <a href="http://379m.com/1">mooshu</a> written by steve "<a href="http://ultramookie.com">mookie</a>" kong.<br />
+<a href="http://379m.com/1">mooshu</a> is distributed under <a href="http://www.gnu.org/licenses/gpl-3.0.html">gplv3</a>.<br />
 </body>
 </html>
