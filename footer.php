56918dbe05cc5194dc7a41fecf4cdb38d7bd736f
steve mookie kong <mookie@ymail.com>
2013-03-29 09:35:55 -0700

    updated links in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <hr />
-powered by <a href="http://379m.com/1">mooshu</a> written by steve "<a href="http://mookiesplace.com">mookie</a>" kong.<br />
-<a href="http://379m.com/1">mooshu</a> is distributed under <a href="http://www.gnu.org/licenses/gpl-3.0.html">gplv3</a>.<br />
+powered by <a href="http://yllw.org/1">mooshu</a> written by steve "<a href="http://hellomookie.com">mookie</a>" kong.<br />
+<a href="http://yllw.org/1">mooshu</a> is distributed under <a href="http://www.gnu.org/licenses/gpl-3.0.html">gplv3</a>.<br />
 </body>
 </html>
